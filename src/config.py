61ae0c4bfac19e729a8f4237467f3f380cc3712b
stalinEPN <156889530+stalinEPN@users.noreply.github.com>
2024-01-17 10:18:24 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 class Config:
-    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+    SECRET_KEY = ''
     
 class DevelompmentConfig(Config):
     DEBUG=True
-    MYSQL_HOST = 'mysql-stalinserver.alwaysdata.net'
-    MYSQL_USER = '286297'
-    MYSQL_PASSWORD = 'stalin21r21'
-    MYSQL_DB = 'stalinserver_inalambricos'
+    MYSQL_HOST = ''
+    MYSQL_USER = ''
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = ''
 
 config={
     'development':DevelompmentConfig
